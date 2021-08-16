993fbda0c46f58bd98bf3e3a1d9a5c5191002150
Daniel Urdaneta <daniel.urdaneta02@gmail.com>
2021-08-16 16:32:13 -0500

    new features added

--- js/app.js
+++ js/app.js
@@ -1,30 +1,36 @@
 
 imgData = aqPhotos
 DescData = aqDescription
+inhabData = inhabitants
+
 function handleClick(){
     console.log('button is working')
     let date = d3.select("#datetime").property('value');
     let imgFilteredData = imgData;
     let descFilteredData = DescData
+    let inhabFilteredData = inhabData
 
     if (date) {
         imgFilteredData = imgFilteredData.filter(row => row.datetime === date);
         descFilteredData = descFilteredData.filter(row => row.datetime === date)
+        inhabFilteredData = inhabFilteredData.filter(row => row.datetime === date)
     }
     else{
         imgFilteredData = []
         descFilteredData = []
+        inhabFilteredData = []
     };
    
-    
     printImg(imgFilteredData)
     printDescription(descFilteredData)
+    printInhabitants(inhabFilteredData)
 }
 
-var imgList = d3.select('.product')
+
 
 function printImg(data){
-console.log(data)
+
+    var imgList = d3.select('.product')
     imgList.html("")
     data.forEach((row)=> {
         imgList
@@ -34,13 +40,43 @@ console.log(data)
         .attr('onClick', 'change(this)')
     })
 
+}
 
+
+function change(img){
+    var src = img.getAttribute('src')
+    console.log(src)
+    selector2 = d3.select('#big')
+    selector2
+        .attr('src', src )
+        .attr('class', 'rounded black-border border-dark')
+    document.getElementById('big').style.height = "590px"
+    document.getElementById('big').style.width = "470px"
+}
+
+
+
+function printDescription(data){
+
+    monthDesc = d3.select('#description')
+
+    monthDesc.html("")
+
+    data.forEach((row) => {
+        monthDesc
+            .append('p')
+            .text(row.description)
+        })
+}
+
+function printInhabitants(data) {
    
-    
+    d3.select('#nav-plant').html('')
+    d3.select('#nav-animal').html('')
 
-    data.forEach((row)=>{
+    data[0].inhabitants.forEach((row)=>{
         
-        if (row.type === 'plant') {
+        if (row.type === 'Plant') {
             tab = d3.select('#nav-plant')
         }
         else {
@@ -51,46 +87,25 @@ console.log(data)
                                 .attr('class', 'container')
 
         var tabRow = tabContainer
-                        .attr('class', 'row fixed-image')
+                        .attr('class', 'row')
 
 
         tabRow
         .append('div')
-        .attr('class', 'col-md-2')
+        .attr('class', 'col-md-3 fixed-image')
             .append('img')
             .attr('src', row.img)
+            .attr('class', 'black-border rounded')
             
         tabRow
         .append('div')
-            .attr('class', 'col-md-10 words')
+            .attr('class', 'col-md-6 words')
         .append('p')
-            .text(row.txt)
+            .html(row.description)
     })
+
 }
 
 d3.selectAll("#filter-btn").on("click", handleClick);
-
-function change(img){
-    var src = img.getAttribute('src')
-    console.log(src)
-    selector2 = d3.select('#big')
-    selector2
-    .attr('src', src )
-    .attr('class', 'rounded black-border border-dark')
-    document.getElementById('big').style.height = "590px"
-    document.getElementById('big').style.width = "470px"
-}
-
-function printDescription(data){
-
-    data.forEach((row) => {
-    desc = d3.select('#description')
-            .append('div')
-            .attr('class', 'col-md-9 words')
-            .append('p')
-            .text(row.description)
-        })
-}
-
 
 
--- js/data.js
+++ js/data.js
@@ -24,3 +24,29 @@ var aqDescription = [{
     datetime:'12/2020',
     description: 'This was the first month of the aquarium, it started with a log, an Amazon plant, and with Sol and Rio, two happy blue Dwarf Gourmis. bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla '
 }]
+
+var inhabitants = [{
+    datetime: '12/2020',
+    inhabitants: [{
+        name: 'Rio',
+        img: "static/images/inhab0/img1.jpg",
+        description: '<span> Name: </span> Rio <br> <span> Specie: </span> Dwarf Gourami <br> <span> Color: </span> Neon blue',
+        type: 'Animal'
+    },
+    {
+        name: 'Sol',
+        img: "static/images/inhab0/img2.jpg",
+        description: '<span> Name: </span> Sol <br> <span> Specie: </span> Dwarf Gourami <br> <span> Color: </span> Neon blue',
+        type: 'Animal'
+    }
+    ]
+},
+{
+    datetime: '01/2021',
+    inhabitants: [{
+        name: 'Paco y Pepe',
+        img: "static/images/images0/img5.jpg",
+        type: 'Plant',
+        description: 'blablablablblabablblablablablablablabablblablablablablablabablblablablablablabla'
+    }]
+}]
