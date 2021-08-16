a3c0e949e0a87f19dd8703c1ed230f920c8caf69
Daniel Urdaneta <daniel.urdaneta02@gmail.com>
2021-08-16 18:59:28 -0500

    features added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 
+// Import datasets
 imgData = aqPhotos
 DescData = aqDescription
 inhabData = inhabitants
 
+// Function created to filter all the dataset and activate the deployment functions
 function handleClick(){
     console.log('button is working')
     let date = d3.select("#datetime").property('value');
@@ -19,15 +21,18 @@ function handleClick(){
         imgFilteredData = []
         descFilteredData = []
         inhabFilteredData = []
+        
+        handleError()
     };
    
+    
     printImg(imgFilteredData)
     printDescription(descFilteredData)
     printInhabitants(inhabFilteredData)
 }
 
 
-
+// Function that print out all the aquarium images for that month
 function printImg(data){
 
     var imgList = d3.select('.product')
@@ -36,13 +41,15 @@ function printImg(data){
         imgList
         .append("img")
         .attr('src', row.img)
+        .attr('id', 'imagen')
         .attr('class', 'black-border rounded')
         .attr('onClick', 'change(this)')
     })
 
+    document.getElementById('imagen').click()
 }
 
-
+// Function that expand image size when selected 
 function change(img){
     var src = img.getAttribute('src')
     console.log(src)
@@ -55,11 +62,10 @@ function change(img){
 }
 
 
-
+//Function that print the description of the selected month
 function printDescription(data){
 
     monthDesc = d3.select('#description')
-
     monthDesc.html("")
 
     data.forEach((row) => {
@@ -69,6 +75,7 @@ function printDescription(data){
         })
 }
 
+// Function that print out all the inhabitant's pictures and descriptions for that specific month
 function printInhabitants(data) {
    
     d3.select('#nav-plant').html('')
@@ -106,6 +113,42 @@ function printInhabitants(data) {
 
 }
 
+// Function that initialize all functions to have a layout of the first month when the page is loaded
+function init(){
+
+    let imgFilteredData = imgData
+    let descFilteredData = DescData
+    let inhabFilteredData = inhabData
+
+    imgFilteredData = imgFilteredData.filter(row => row.datetime === '12/2020');
+    descFilteredData = descFilteredData.filter(row => row.datetime === '12/2020')
+    inhabFilteredData = inhabFilteredData.filter(row => row.datetime === '12/2020')
+
+    printImg(imgFilteredData)
+    printDescription(descFilteredData)
+    printInhabitants(inhabFilteredData)
+
+    document.getElementById('imagen').click()
+
+}
+
+function handleError(){
+    title = d3.select('#news')
+    title.html("")
+
+    layout = d3.select('#layout')
+    layout.html("")
+    layout
+        .append('div')
+        .attr('class', 'col-md-12 words')
+        .append('p')
+        .html('<h1> Sorry, no photos found :( </h1>')
+}
+
+// Initialize init function
+init()
+
+// Event listener
 d3.selectAll("#filter-btn").on("click", handleClick);
 
 
--- js/data.js
+++ js/data.js
@@ -17,12 +17,40 @@ var aqPhotos = [{
 {
     datetime: "12/2020",
     img: "static/images/images0/img5.jpg"
+},
+{
+    datetime: "01/2021",
+    img: "static/images/images1/img1.jpg"
+},
+{
+    datetime:"01/2021",
+    img: "static/images/images1/img2.jpg"
+},
+{
+    datetime: "01/2021",
+    img: "static/images/images1/img3.jpg"
+},
+{
+    datetime: "01/2021",
+    img: "static/images/images1/img4.jpg"
+},
+{
+    datetime: "01/2021",
+    img: "static/images/images1/img5.jpg"
+},
+{
+    datetime: "01/2021",
+    img: "static/images/images1/img6.jpg"
 }
 ] 
 
 var aqDescription = [{
     datetime:'12/2020',
-    description: 'This was the first month of the aquarium, it started with a log, an Amazon plant, and with Sol and Rio, two happy blue Dwarf Gourmis. bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla '
+    description: 'This was the first month of the aquarium, it started with a log, an Amazon Sword plant, and with Sol and Rio, two happy blue Dwarf Gourmis.'
+},
+{
+    datetime:'01/2021',
+    description: 'In the second month of the aquarium we still see Rio and Sol getting used to their environment , we also see the Amazon Sword growing up with a few more leaves, and we added the  "Monte Carlo" plant in the foreground.'
 }]
 
 var inhabitants = [{
@@ -44,9 +72,16 @@ var inhabitants = [{
 {
     datetime: '01/2021',
     inhabitants: [{
-        name: 'Paco y Pepe',
-        img: "static/images/images0/img5.jpg",
-        type: 'Plant',
-        description: 'blablablablblabablblablablablablablabablblablablablablablabablblablablablablabla'
-    }]
+        name: 'Rio',
+        img: "static/images/inhab0/img1.jpg",
+        description: '<span> Name: </span> Rio <br> <span> Specie: </span> Dwarf Gourami <br> <span> Color: </span> Neon blue',
+        type: 'Animal'
+    },
+    {
+        name: 'Sol',
+        img: "static/images/inhab0/img2.jpg",
+        description: '<span> Name: </span> Sol <br> <span> Specie: </span> Dwarf Gourami <br> <span> Color: </span> Neon blue',
+        type: 'Animal'
+    }
+    ]
 }]
